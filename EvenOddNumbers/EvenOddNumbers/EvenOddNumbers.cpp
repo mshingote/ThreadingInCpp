4ef26a652fc884036b211b78ba10cac24fd478fa
Mayur Shingote <mayur.shingote@yahoo.com>
2023-01-08 14:55:05 +0530

    2 Thread Synchronization to print event and odd numbers

---
+++ EvenOddNumbers/EvenOddNumbers/EvenOddNumbers.cpp
@@ -0,0 +1,58 @@
+// EvenOddNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <cassert>
+
+static constexpr uint64_t k_MaxNumber = 100;
+
+struct ThreadContext
+{
+	static std::mutex s_Lock[2];
+	static std::condition_variable s_Cv[2];
+	static uint64_t s_Number;
+};
+std::mutex ThreadContext::s_Lock[2];
+std::condition_variable ThreadContext::s_Cv[2];
+uint64_t ThreadContext::s_Number;
+
+static bool IsNumberReachedAtMax()
+{
+	return ThreadContext::s_Number >= k_MaxNumber;
+}
+
+void ThreadFunction(ThreadContext& Context, uint8_t Reminder)
+{
+	while (!IsNumberReachedAtMax())
+	{
+		std::unique_lock<std::mutex> lock(Context.s_Lock[Reminder]);
+		ThreadContext::s_Cv[Reminder].wait(lock, [&Context , &Reminder]()
+			{
+				if (IsNumberReachedAtMax())
+				{
+					return true;
+				}
+				return Context.s_Number % 2 == Reminder;
+			}
+		);
+		if (IsNumberReachedAtMax())
+		{
+			return;
+		}
+		std::clog << (Reminder ? "\t\t\t\t\t" : "") << "\tCurrent thread: " << std::this_thread::get_id() << ", Number: " << Context.s_Number << "\n";
+		++Context.s_Number;
+		ThreadContext::s_Cv[!Reminder].notify_one();
+	}
+}
+int main()
+{
+	ThreadContext contextForEvent, contextForOdd;
+	std::thread EvenNumberProcessThread(ThreadFunction, std::ref(contextForEvent), 0);
+	std::thread OddNumberProcessThread(ThreadFunction, std::ref(contextForOdd), 1);
+
+	EvenNumberProcessThread.join();
+	OddNumberProcessThread.join();
+}
